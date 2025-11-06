35916142b531962a4c693eccaae9a9198458933d
Sergei Orlov <sorlov+github@gmail.com>
2025-11-06 09:36:32 -0800

    gcloud working ryba init

--- setup_gcloud.sh
+++ setup_gcloud.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+set -e
 sudo apt-get update
-sudo apt install python3-pip -y
-pip3 install numpy librosa matplotlib dill micrograd --break-system-packages
-time python3 learn_multi_voices.py
+sudo apt install python3-pip python3-venv -y
+# pip3 install setuptools distutils --break-system-packages
+python3 -m venv venv
+venv/bin/pip3 install torch numpy matplotlib librosa dill micrograd
+time venv/bin/python3 test_reconstruction_deep.py
--- test_reconstruction_deep.py
+++ test_reconstruction_deep.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn.functional as F
 import os
 
-this_file = os.path.basename(__file__)
+this_file = os.path.basename(__file__).split('.')[0]
 
 datafile, sr = librosa.load('andrej.au', sr=None)
 spec = librosa.stft(datafile, n_fft=2048, hop_length=2048 // 2)
@@ -18,9 +18,6 @@ data = []
 for t in range(len(mag[0])):
     t_slice = [x[t] for x in mag]
     data.append(t_slice[10:600])
-
-# for j, o in enumerate(data):
-#     data[j] = [freq if i < 600 and i > 10 else 0 for i, freq in enumerate(o)]
 
 xs = torch.Tensor(np.array(data[:-1]))
 ys = torch.Tensor(np.array(data[1:]))
@@ -34,18 +31,22 @@ W2 = torch.randn(f_size, f_size, requires_grad=True)
 B2 = torch.randn(f_size, requires_grad=True)
 eps = 1e-32
 
-epochs = 5_000
+epochs = 10_000
 model_file = f'W{epochs}.pkl-{this_file}'
 try:
+    # assert False
     state = pickle.load(open(model_file, 'rb'))
     print(f'read model {model_file}')
     (W1, B1, W2, B2) = state
 except:
     for j in range(epochs):
-        rate = 2
+        rate = 1
         loss = 0
         # Forward pass
-        logits = (xs @ W1 + B1).relu() @ W2 + B2
+        # logits = xs @ W1 + B1
+        logits = (xs @ W1 + B1)
+        logits = logits.tanh()
+        logits = logits @ W2 + B2
         loss = ((logits - ys)**2).mean()
 
         # Backward pass
@@ -66,42 +67,25 @@ except:
     pickle.dump(state, open(model_file, 'wb'))
     print(f'wrote model {model_file}')
 
-print(W1)
-print(B1)
-print(W2)
-print(B2)
-assert False
-
 W1 = W1.detach()
 B1 = B1.detach()
 W2 = W2.detach()
 B2 = B2.detach()
 
-os = (xs @ W1 + B1).relu() @ W2 + B2
-os = torch.cat((xs[0].unsqueeze(0), os), dim=0)
+# os = (xs @ W1 + B1).tanh() @ W2 + B2
+# os = torch.cat((xs[0].unsqueeze(0), os), dim=0)
 
-# os = [xs[0]]
-# for i in range(1, t_size):
-#   os.append(os[i-1] @ W.detach())
-#os = xs
-
-# os = xs
+os = [xs[0]]
+for i in range(1, t_size):
+  os.append((os[i-1] @ W1 + B1).tanh() @ W2 + B2)
 
 
-
-# os = os[:len(os)//2]
-# ys = os[:len(ys)//2]
-
-# os = data
-# for j, o in enumerate(os):
-#     os[j] = [freq if i < 600 and i > 10 else 0 for i, freq in enumerate(o)]
-
-for t in range(t_size):
-    plt.figure(figsize=(20,12))
-    plt.plot(os[t], c='r')
-    plt.plot(ys[t], c='g', alpha=.6)
-    plt.show()
-
+# for t in range(t_size):
+#     plt.figure(figsize=(20,12))
+#     plt.plot(os[t], c='r')
+#     plt.plot(ys[t], c='g', alpha=.6)
+#     plt.show()
+# 
 out_spec = []
 for f in range(f_size):
     f_slice = [0] * 10
