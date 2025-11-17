67aea7874cfeaf02dc111975ee7803a3d1791458
Sergei Orlov <sorlov+github@gmail.com>
2025-11-16 18:14:35 -0800

    ah.au sounds good, but robotic loss=.67 after 100k ticks

--- test_reconstruction.py
+++ test_reconstruction.py
@@ -10,7 +10,7 @@ import os
 
 this_file = os.path.basename(__file__)
 
-datafile, sr = librosa.load('ma.au', sr=None)
+datafile, sr = librosa.load('ah.au', sr=None)
 spec = librosa.stft(datafile, n_fft=2048, hop_length=2048 // 2)
 mag, phase = librosa.magphase(spec)
 
@@ -18,9 +18,6 @@ data = []
 for t in range(len(mag[0])):
     t_slice = [x[t] for x in mag]
     data.append(t_slice[10:600])
-
-# for j, o in enumerate(data):
-#     data[j] = [freq if i < 600 and i > 10 else 0 for i, freq in enumerate(o)]
 
 xs = torch.Tensor(np.array(data[:-1]))
 ys = torch.Tensor(np.array(data[1:]))
@@ -37,19 +34,15 @@ epochs = 100000
 model_file = f'W{epochs}.pkl-{this_file}'
 try:
     assert False
+    # Uncomment if you want to enable using saved Ws from pkl files
     # W = pickle.load(open(model_file, 'rb'))
     # print(f'read model {model_file}')
 except:
     for j in range(epochs):
-        rate = .04
+        rate = .03
         loss = 0
-        # Forward pass
-        # loss = F.cross_entropy(xs @ W, ys)
         logits = xs @ W
         loss = ((logits - ys)**2).mean()
-
-        #logits = logits - logits.max()
-        #counts = logits.exp() # + eps
 
         # Backward pass
         W.grad = None
@@ -62,6 +55,8 @@ except:
     pickle.dump(W, open(model_file, 'wb'))
     print(f'wrote model {model_file}')
 
+# In case you want to cheat and predict os using all xs (instead of starting
+# with xs[0] and generating 1 frame at a time:
 # os = xs @ W.detach()
 # os = torch.cat((xs[0].unsqueeze(0), os), dim=0)
 
@@ -69,25 +64,12 @@ os = [xs[0]]
 for i in range(1, t_size):
   os.append(os[i-1] @ W.detach())
 
-# os = xs
-
-# os = data
-# for j, o in enumerate(os):
-#     os[j] = [freq if i < 600 and i > 10 else 0 for i, freq in enumerate(o)]
-
-# for t in range(t_size):
-#     plt.figure(figsize=(20,12))
-#     plt.plot(os[t], c='r')
-#     plt.plot(ys[t], c='g', alpha=.6)
-#     plt.show()
-
 out_spec = []
 for f in range(f_size):
     f_slice = [0] * 10
     for t in range(t_size):
+        # Arguably, we're still cheating by using the original phase
         f_slice.append(complex(os[t][f],0) * phase[f][t])
-        # f_slice.append(complex(os[t][f],0) * 1)
-        #t_slice.append(os[t][f])
     f_slice += [0] * (1024 - 600)
     out_spec.append(f_slice)
 
